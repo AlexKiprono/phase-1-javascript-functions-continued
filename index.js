7e0abc0bad683531d784708327136c83b86c2e47
AlexKiprono <lexiekiprono@gmail.com>
2024-03-21 11:56:38 +0300

    functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// code your solution here
+//function saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    console.log(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+//no arguments are passed
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//mondayWork
+
+// function mondayWork(activity = 'go to the office.') {
+//   return `This Monday, I will ${activity}.`;
+// }
+// mondayWork("work from home");
+
+//no arguments are passed
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+//overriden
+
+// function mondayWork() {
+//   console.log (`This Monday, I will work from home`);
+// }
+
+function wrapAdjective(symbol ='*') {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+      }
+}
+
+
+
+  
